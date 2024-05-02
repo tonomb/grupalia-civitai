91f736da98bb8c8eba8dc91cadfdb04a60d04694
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-05-02 15:11:46 -0600

    created ImageGrid

---
+++ src/components/ImageGrid/ImageGrid.spec.tsx
@@ -0,0 +1,8 @@
+import ImageGrid from "./ImageGrid";
+import { render } from "@testing-library/react";
+
+describe("<ImageGrid />", () => {
+  it("should render the image grid", () => {
+    render(<ImageGrid />);
+  });
+});
