c3de86a8a91e6f606c1d680392bbb40d5d64f580
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-05-02 15:06:18 -0600

    completed navbar

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Logo() {
   return (
-    <div className="w-[150px]">
+    <div className="w-[140px] flex flex-row justify-center items-center">
       <img src="/assets/logo-dark.png" alt="civitai logo" />
     </div>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,8 +3,25 @@ import Logo from "../Logo/Logo";
 
 export default function Navbar() {
   return (
-    <div data-testid="navbar">
+    <div data-testid="navbar" className="p-4 flex flex-row gap-2 align-center">
       <Logo />
+      <div className="border border-solid border-[#51CF66] text-[#51CF66] rounded py-1 px-2 flex flex-row">
+        <p>By Tono MB </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-chevron-right"
+        >
+          <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
+      </div>
     </div>
   );
 }
